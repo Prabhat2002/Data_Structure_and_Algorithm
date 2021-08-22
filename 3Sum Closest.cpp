8b3ad99933e71415d3fb713e381bdd3b31e5569d
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-08-22 23:46:31 +0530

    Update 3Sum Closest.cpp

--- 3Sum Closest.cpp
+++ 3Sum Closest.cpp
@@ -2,6 +2,41 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) 
     {
+        int n=nums.size();
+        sort(begin(nums), end(nums));
+        int result = 0, min_diff = INT_MAX;
+        for (int i = 0; i<n-1; i++) 
+        {
+            int left = i+1, right = n - 1;
+            while (left < right) 
+            {
+                const auto& total = nums[left] + nums[right] + nums[i];
+                if (total < target) 
+                {
+                    ++left;
+                } 
+                else if (total > target) 
+                {
+                    --right;
+                } 
+                else 
+                {
+                    return target;
+                }
+                if (abs(total - target) < min_diff)
+                {
+                    min_diff = abs(total - target);
+                    result = total;
+                }
+            }
+        }
+        return result;
+    }
+};
+/*class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
         sort(begin(nums), end(nums));
         int result = 0, min_diff = numeric_limits<int>::max();
         for (int i = size(nums) - 1; i >= 2; --i) {
