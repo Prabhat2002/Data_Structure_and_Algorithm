46325e64a6f2d1d2d5f838826c093ef0532ae6e3
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-08-24 17:24:05 +0530

    Create Remove Duplicates from Sorted List.cpp

---
+++ Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+     int removeDuplicates(vector<int>& nums) 
+     {
+        if (nums.empty()) 
+        {
+            return 0;
+        }
+
+        const int k = 2; 
+
+        int left = 0;
+        int right = 1;
+
+        while (right < nums.size()) 
+        {
+            if (nums[left] != nums[right] || 
+                (left - k + 1 < 0 || nums[left] != nums[left - k + 1])) 
+            {
+                ++left;
+                nums[left] = nums[right];
+            }
+            ++right;
+        }
+
+        return left + 1;   
+    }
+};
