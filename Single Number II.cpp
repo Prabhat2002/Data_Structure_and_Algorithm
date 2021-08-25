302cd0060066b36279812207a7fe318a0170e307
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-08-25 23:06:17 +0530

    Create Single Number II.cpp

---
+++ Single Number II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int one = 0;
+        int two = 0;
+        int three = 0;
+        for (int i = 0; i < nums.size(); i ++)
+        {
+            two = two | ( one & nums[i]);
+            one = one ^ nums[i];
+            three = one & two;
+            one = one & (~three);
+            two = two & (~three);
+        }
+        return one;
+    }
+};
