e3e14ef70a92465e68346a6feea9567b8f2a6687
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-09-07 00:35:21 +0530

    Create Move Zeros.cpp

---
+++ Move Zeros.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& a) 
+    {   
+        int i=0,j=0;
+        int t=a.size()-1;
+        while(i<=t)
+        {
+            if(a[i]!=0)
+            {
+                a[j++]=a[i]; 
+            }
+            i++;
+        }
+        j=t-j+1;
+        while(j!=0)
+        {
+            a[t--]=0;
+            j--;
+        }
+    }     
+};
+  
