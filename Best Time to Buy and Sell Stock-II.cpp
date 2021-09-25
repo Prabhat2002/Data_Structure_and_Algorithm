d7a55cee991c59a040303ad3691e9383abd9bdee
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-09-25 18:34:48 +0530

    Create Best Time to Buy and Sell Stock-II.cpp

---
+++ Best Time to Buy and Sell Stock-II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+      int len=prices.size();
+        if(len <= 1) 
+            return 0;
+        int sum = 0;
+        for(int i = 1; i < len; i++) 
+        {
+            if(prices[i] - prices[i - 1] > 0) 
+               sum += prices[i] - prices[i - 1];
+        }
+
+        return sum;
+    }
+};
