23430b1679ff363da43ff725cea12c4dbe583ce6
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-10-06 18:41:48 +0530

    Create Copy List with Random Pointer.cpp

---
+++ Copy List with Random Pointer.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        for (auto *curr = head; curr; curr = curr->next->next) 
+        {
+            auto *node = new Node(curr->val);
+            node->next = curr->next;
+            curr->next = node;
+        }
+        for (auto *curr = head; curr; curr = curr->next->next) 
+        {
+            if (curr->random)
+                curr->next->random = curr->random->next;
+        }
+        Node dummy(0);
+          for (auto *curr = head, *copy_curr = &dummy;curr;
+             copy_curr = copy_curr->next, curr = curr->next)
+          {
+            copy_curr->next = curr->next;
+            curr->next = curr->next->next;
+          }
+        return dummy.next;
+    }
+};
