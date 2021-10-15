d9781029843b546f1f492ec8b68c1e8f39624791
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-10-16 01:46:40 +0530

    Create Complex Number Multiply.cpp

---
+++ Complex Number Multiply.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string complexNumberMultiply(string a, string b)
+    {
+        int ra, ia, rb, ib;
+        char op;
+        stringstream ssa(a), ssb(b);
+        ssa >> ra >> op >> ia;
+        ssb >> rb >> op >> ib;
+        string result = to_string(ra * rb - ia * ib);
+        result += "+";
+        result += to_string(ra * ib + rb * ia);
+        result += "i";
+        return result;    
+    }
+};
