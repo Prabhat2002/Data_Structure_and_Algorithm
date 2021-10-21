9c559861a910b08e6d2013381e11f086d1806a5f
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-10-22 01:20:29 +0530

    Create H-Index.cpp

---
+++ H-Index.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+  public:
+    int hIndex(vector<int>& v) 
+    {
+        sort(v.begin(),v.end());
+        int n=v.size();
+        int l=0;
+        int r=n-1;
+        while (l<=r) 
+        {
+            int mid=(l+r)/2;
+            if(v[mid]>=n-mid) 
+                r=mid-1;
+            else 
+                l=mid+1;
+        }
+        return n-l;
+    }
+};
