e78dc0aa23ac5aa9308e63c31c8ce500f3901ee7
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-10-23 17:29:01 +0530

    Create 132 Pattern.java

---
+++ 132 Pattern.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean find132pattern(int[] nums) 
+    {
+         int n = nums.length;
+            int[] min = new int[n];
+            min[0] = nums[0];
+            for (int i = 1; i < n; i++)
+                min[i] = Math.min(min[i - 1], nums[i]);
+
+            Stack<Integer> stack = new Stack<>();
+            for (int j=nums.length-1;j>=0;j--) 
+            {
+                if (nums[j]>min[j]) 
+                {
+                    while(!stack.isEmpty()&&stack.peek()<=min[j])
+                        stack.pop();
+                    if (!stack.isEmpty()&&stack.peek()<nums[j])
+                        return true;
+                    stack.push(nums[j]);
+                }
+            }
+            return false;
+    }
+}
+
+/*
+nums : 3 1 4 2
+min : 3 1 1 1
+ if 2>1
+   st.push(2)
+   
+*/
