a5d33de5e8db80ec66ffd4b2e57a36e3115b1ec2
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-11-01 16:19:44 +0530

    Create Find Positive Integer Solution for a Given Equation.cpp

---
+++ Find Positive Integer Solution for a Given Equation.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+ public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) 
+    {
+        vector<vector<int>> res;
+        for(int x=1;x<=1000;x++)
+        {
+           for(int y=1;y<=1000;y++)
+           {
+                if(customfunction.f(x,y)==z)
+                    res.push_back({x,y});
+           }
+        }
+       return res;
+    }
+};
