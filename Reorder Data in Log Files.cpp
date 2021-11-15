e7e9cee6e10ae109c870f0d5aa08578d2063e166
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-11-15 20:36:18 +0530

    Create Reorder Data in Log Files.cpp

---
+++ Reorder Data in Log Files.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) 
+    {
+     stable_sort(begin(logs), end(logs), [](auto &a, auto &b) {
+            bool aa = isalpha(a.back());
+            bool bb = isalpha(b.back());
+            if (aa && bb) {
+                int i = a.find(' ');
+                int j = b.find(' ');
+                string w1 = a.substr(i + 1);
+                string w2 = b.substr(j + 1);
+                string i1 = a.substr(0, i);
+                string i2 = b.substr(0, j);
+                return w1 == w2 ? i1 <= i2 : w1 < w2;
+            }
+            if (aa && (!bb)) {
+                return true;                
+            }
+            return false;
+        });
+        return logs;
+    }
+};
