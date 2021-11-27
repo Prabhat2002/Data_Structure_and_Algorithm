7baff9e514421e2b702f6e6bc5f022a418804bd9
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-11-27 11:33:40 +0530

    Create Build Array from Permutation.cpp

---
+++ Build Array from Permutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) 
+    {
+         int n=nums.size();
+         vector<int>v(n);  
+         for(int i=0;i<n;i++)
+             v[i]=nums[nums[i]];
+         return v;
+    }
+};
