ffb0ed6ac8b7961964b6b35727467b4cb7066c00
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-11-28 18:28:38 +0530

    Create 1-bit and 2-bit Characters.cpp

---
+++ 1-bit and 2-bit Characters.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+ public:
+    bool isOneBitCharacter(vector<int>& bits) 
+    {
+        int n=bits.size();
+        int i=0;
+        while(i<n-1) 
+        {
+            if(bits[i]==0) 
+                i++;
+            else 
+                i+=2;
+        }
+        return i==n - 1;
+    }
+};
