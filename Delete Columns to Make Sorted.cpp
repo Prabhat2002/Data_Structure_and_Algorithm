dceddfab6b9434a9dfe2388732678d3ccc9fbd0c
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2021-12-03 11:36:30 +0530

    Create Delete Columns to Make Sorted.cpp

---
+++ Delete Columns to Make Sorted.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+ public:
+    int minDeletionSize(vector<string>& strs) 
+    {
+        int result=0;
+        for (int i=0;i<strs[0].size();++i) 
+        {
+            for (int j=1;j<strs.size();++j) 
+            {
+                if(strs[j-1][i]>strs[j][i])
+                {
+                    ++result;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
