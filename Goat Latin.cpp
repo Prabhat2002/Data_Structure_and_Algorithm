3651013e8d0fd0e7f92f61fef85ab06b1de51ea9
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-01-23 23:26:15 +0530

    Create Goat Latin.cpp

---
+++ Goat Latin.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string toGoatLatin(string S) 
+    {
+        set<char> vowel{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        string result, word;
+        int count = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            if (S[i] != ' ') {
+                word.push_back(S[i]);
+                if (i != S.length() - 1) {
+                    continue;
+                }
+            }
+            if (!vowel.count(word.front())) {
+                word.push_back(word.front());
+                word = word.substr(1);
+            }
+            word += "ma";
+            word += string(++count, 'a');
+            result += word;
+            result += " ";
+            word.clear();
+        }
+        result.pop_back();
+        return result;
+    }
+};
