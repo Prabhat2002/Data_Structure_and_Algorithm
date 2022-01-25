3437d45a73e44cad07ad20c81021f4a6335b7f17
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-01-25 12:13:12 +0530

    Create Water and Jug Problem.cpp

---
+++ Water and Jug Problem.cpp
@@ -0,0 +1,11 @@
+class Solution 
+{
+ public:
+    bool canMeasureWater(int x, int y, int z) {
+        return z == 0 || (x + y >= z && z % gcd(x, y) == 0);
+    }
+    private: 
+    int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+};
