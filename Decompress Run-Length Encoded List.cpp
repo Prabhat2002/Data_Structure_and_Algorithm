b1f9288d9b30617ad111ad80ea8cdfb313f3ab25
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-01-31 15:11:26 +0530

    Create Decompress Run-Length Encoded List.cpp

---
+++ Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+ public:
+    vector<int> decompressRLElist(vector<int>& nums) 
+    {
+        vector<int>v;
+        for(int i=0;i<nums.size();i=i+2)
+        {
+            for(int j=0;j<nums[i];j++)
+            {
+                v.push_back(nums[i+1]);
+            }
+        }
+        return v;
+    }
+};
