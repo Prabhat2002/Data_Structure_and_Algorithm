6607c48514cbe7c2a055e45ad2498b4a50b581ed
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-03-06 20:38:43 +0530

    Create Next Greater Element I(Stack).cpp

---
+++ Next Greater Element I(Stack).cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+ public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
+    {
+         stack<int>st;
+        vector<int>v;
+        unordered_map<int,int>mp;
+        for(int i=nums2.size()-1;i>=0;i--)
+        {
+           while(!st.empty()&&st.top()<nums2[i])
+               st.pop();
+           if(st.empty())
+               mp[nums2[i]]=-1;
+            else
+                mp[nums2[i]]=st.top();
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++)
+        {
+            v.push_back(mp[nums1[i]]);
+     }
+        return v;       
+    }
+};
