d2d0142fa627f92345de80f2ab9fd05e103266a3
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-03-07 15:46:53 +0530

    Create The kth Factor of n.cpp

---
+++ The kth Factor of n.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+ public:
+    int kthFactor(int n, int k) 
+    {
+       vector<int>v;
+       for(int i=1;i<=n;i++)
+       {
+           if(n%i==0)
+               v.push_back(i);
+           if(v.size()==k)
+               return v.back();
+       }
+       return -1;
+    }
+};
